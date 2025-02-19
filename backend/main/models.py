62ca5f6036f5ea7a8722afe411a8500763559c1d
Nikita <k_azz@bk.ru>
2025-02-19 21:48:09 +0400

    update number cus

--- backend/main/models.py
+++ backend/main/models.py
@@ -64,7 +64,7 @@ class Customer(models.Model):
     customer_surname = models.CharField(max_length=50)
     customer_name = models.CharField(max_length=50)
     customer_patronymic = models.CharField(max_length=50, null=True, blank=True)
-    customer_phone_number = models.CharField(max_length=20)
+    customer_phone_number = models.CharField(max_length=12)
 
     def __str__(self):
         return f"{self.customer_surname} {self.customer_name}"
