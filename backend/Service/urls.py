e648ede6112044509d03f3599f8fa1579e3c64de
Shikin Nikita <k_azz@bk.ru>
2025-03-23 23:45:35 +0400

    API UPDATE

--- backend/Service/urls.py
+++ backend/Service/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ServiceRequestCreateView, ServiceDetailListView, TypeOfServiceListView
-
+from .views import ServiceRequestCreateView, ServiceDetailListView, TypeOfServiceListView, CancelServiceRequestView, UpdateServiceRequestStatusView, ServiceRequestIDView
 urlpatterns = [
-    path('requests', ServiceRequestCreateView.as_view(), name='Service-Request-create'),
+    path('requests', ServiceRequestCreateView.as_view(), name='Service-Request'),
+    path('requests/<int:id>/', ServiceRequestIDView.as_view(), name='Service-Request-ID'),
+    path('requests/<int:id>/cancel', CancelServiceRequestView.as_view(), name='Сancel_request'),
+    path('requests/<int:id>/status', UpdateServiceRequestStatusView.as_view(), name='Update_status_request'),
     path('details', ServiceDetailListView.as_view(), name='Service-Details-list'),
     path('types', TypeOfServiceListView.as_view(), name='Service-Types-list'),
 ]
--- backend/organization/urls.py
+++ backend/organization/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddressTypeListCreateView, OrganizationListCreateView, AddressListCreateView, OrganizationDetailView
+from .views import AddressTypeListCreateView, OrganizationListCreateView, AddressListCreateView, OrganizationDetailView,CityListView
 
 urlpatterns = [
     path('address-types/', AddressTypeListCreateView.as_view(), name='address-type-list-create'),
     path('', OrganizationListCreateView.as_view(), name='organization-list-create'),
     path('addresses/', AddressListCreateView.as_view(), name='address-list-create'),
     path('<int:pk>/', OrganizationDetailView.as_view(), name='organization-detail'),
+    path('City', CityListView.as_view(), name='City'),
 ]
