9791c0dd7c109ec844d23a29585b5db9057fa30a
Mugz88 <mugz_play@rambler.ru>
2025-03-24 14:32:58 +0400

    add new

--- backend/Service/serializers.py
+++ backend/Service/serializers.py
@@ -4,40 +4,6 @@ from Customer.models import Customer
 from organization.models import Organization, Address, AddressType
 
 class ServiceRequestSerializer(serializers.ModelSerializer):
-<<<<<<< HEAD
-    customer = serializers.PrimaryKeyRelatedField(queryset=Customer.objects.all())
-    organization = serializers.PrimaryKeyRelatedField(queryset=Organization.objects.all())
-    service_details = serializers.SerializerMethodField()
-
-    class Meta:
-        model = ServiceRequest
-        fields = [
-            'customer',
-            'organization',
-            'service_details',
-            'date_service',
-            'add_info'
-        ]
-    print(service_details)
-
-    def get_service_details(self, obj):
-        details = ServiceRequestDetail.objects.filter(service_request=obj)
-        return [detail.service_detail_id for detail in details]
-
-    def create(self, validated_data):
-        service_details = validated_data.pop('service_details')
-        service_request = ServiceRequest.objects.create(**validated_data)
-
-        for service_detail_id in service_details:
-            ServiceRequestDetail.objects.create(
-                service_request=service_request,
-                service_detail_id=service_detail_id
-            )
-        
-
-        return service_request
-
-=======
     class Meta:
         model = ServiceRequest
         fields = ['id', 'customer', 'organization', 'date_service', 'add_info', 'status']
@@ -46,7 +12,6 @@ class ServiceRequestDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceRequestDetail
         fields = ['id', 'service_request', 'service_detail']
->>>>>>> e648ede6112044509d03f3599f8fa1579e3c64de
     
 
 
--- backend/Service/urls.py
+++ backend/Service/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
 from .views import ServiceRequestCreateView, ServiceDetailListView, TypeOfServiceListView, CancelServiceRequestView, UpdateServiceRequestStatusView, ServiceRequestIDView
 urlpatterns = [
-<<<<<<< HEAD
-    path('requests/', ServiceRequestCreateView.as_view(), name='Service-Request-create'),
-    path('details/', ServiceDetailListView.as_view(), name='Service-Details-list'),
-    path('types/', TypeOfServiceListView.as_view(), name='Service-Types-list'),
-=======
     path('requests', ServiceRequestCreateView.as_view(), name='Service-Request'),
     path('requests/<int:id>/', ServiceRequestIDView.as_view(), name='Service-Request-ID'),
     path('requests/<int:id>/cancel', CancelServiceRequestView.as_view(), name='Сancel_request'),
     path('requests/<int:id>/status', UpdateServiceRequestStatusView.as_view(), name='Update_status_request'),
     path('details', ServiceDetailListView.as_view(), name='Service-Details-list'),
     path('types', TypeOfServiceListView.as_view(), name='Service-Types-list'),
->>>>>>> e648ede6112044509d03f3599f8fa1579e3c64de
 ]
